3ab068765bd68c8d8dde32acabce5bcfe3bfbb96
Nikolas Dion Susanto <nikolasdion@gmail.com>
2019-05-05 03:22:04 +0100

    Make database Singleton

--- app/src/main/java/com/nikolasdion/moodjournal/EntryDatabase.kt
+++ app/src/main/java/com/nikolasdion/moodjournal/EntryDatabase.kt
@@ -1,9 +1,22 @@
 package com.nikolasdion.moodjournal
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Entry::class], version = 1)
 abstract class EntryDatabase : RoomDatabase() {
     abstract fun entryDao(): EntryDao
+
+    companion object {
+        @Volatile
+        private var instance : EntryDatabase? = null
+
+        fun getEntryDatabase(context : Context) : EntryDatabase {
+            return instance ?: synchronized(this) {
+                Room.databaseBuilder(context, EntryDatabase::class.java, "entry-db").build()
+            }
+        }
+    }
 }
